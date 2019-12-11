1ba1353a3034e601f711d45a70516763c98ed13a
Gegenwarth <ingo.gegenwarth@sap.com>
2019-12-11 11:00:31 +0100

    Create HelperFunctions.ps1

---
+++ HelperFunctions.ps1
@@ -0,0 +1,617 @@
+function Search-UnifiedLog
+{
+    [CmdletBinding()]
+    param(
+        [System.String[]]
+        $UserIDs,
+
+        [System.String]
+        $FreeText,
+
+        [System.String[]]
+        $IPAddresses,
+
+        [System.String[]]
+        $ObjectIds,
+
+        [System.String[]]
+        $Operations,
+
+        [System.String]
+        $RecordType,
+
+        [System.String[]]
+        $SiteIds,
+
+        [System.DateTime]
+        $StartDate = $((Get-Date).AddMonths(-1)),
+
+        [System.DateTime]
+        $EndDate = $(Get-Date),
+
+        [System.String]
+        $SessionID = $(([System.Guid]::NewGuid()).ToString()),
+
+        [System.Int16]
+        $ResultSize = '5000',
+
+        [System.Management.Automation.SwitchParameter]
+        $Formatted
+
+    )
+
+    Begin
+    {
+        $collection = @()
+        [System.Int16]$totalCount = 0
+        [System.Array]$tempResult = $null
+        $timer = [System.Diagnostics.Stopwatch]::StartNew()
+        $param = @{
+            StartDate = $StartDate
+            EndDate = $EndDate
+            SessionId = $SessionID
+            SessionCommand = 'ReturnLargeSet'
+            ResultSize = $ResultSize
+        }
+        if (-not [System.String]::IsNullOrEmpty($UserIDs))
+        {
+            $param.Add('UserIds',$UserIDs)
+        }
+        if (-not [System.String]::IsNullOrEmpty($FreeText))
+        {
+            $param.Add('FreeText',$FreeText)
+        }
+        if (-not [System.String]::IsNullOrEmpty($IPAddresses))
+        {
+            $param.Add('IPAddresses',$IPAddresses)
+        }
+        if (-not [System.String]::IsNullOrEmpty($ObjectIds))
+        {
+            $param.Add('ObjectIds',$ObjectIds)
+        }
+        if (-not [System.String]::IsNullOrEmpty($Operations))
+        {
+            $param.Add('Operations',$Operations)
+        }
+        if (-not [System.String]::IsNullOrEmpty($RecordType))
+        {
+            $param.Add('RecordType',$RecordType)
+        }
+        if (-not [System.String]::IsNullOrEmpty($SiteIds))
+        {
+            $param.Add('SiteIds',$SiteIds)
+        }
+        if ($Formatted)
+        {
+            $param.Add('Formatted',$true)
+        }
+    }
+
+    Process{
+        Do {
+            Write-Verbose "Start searching..."
+            $tempResult = Search-UnifiedAuditLog @param
+            if ($tempResult)
+            {
+                $collection += $tempResult
+                Write-Verbose "TotalCount:$($collection[0].ResultCount) ResultIndex:$($tempResult.ResultIndex[-1]) Runtime:$($timer.Elapsed.ToString())"
+            }
+            else
+            {
+                Write-Verbose 'No records found!'
+            }
+        }
+        Until( $(if ($tempResult){ $tempResult.ResultIndex[-1] -ge $tempResult.ResultCount[-1]} else { return $true}) )
+    }
+
+    End{
+        $collection | Sort-Object CreationDate
+        $timer.Stop()
+        Write-Verbose "ScriptRuntime:$($timer.Elapsed.ToString())"
+    }
+}
+
+function Get-MessageTraceFull
+{
+    [CmdletBinding()]
+    param(
+        [System.DateTime]
+        $EndDate,
+
+        [System.Linq.Expressions.Expression]
+        $Expression,
+
+        [System.String]
+        $FromIP,
+
+        [System.String[]]
+        $MessageId,
+
+        [System.GUID]
+        $MessageTraceId,
+
+        [System.Int32]
+        $PageCount = '1',
+
+        [System.Int32]
+        $PageSize = '5000',
+
+        [System.String]
+        $ProbeTag,
+
+        [System.String[]]
+        $RecipientAddress,
+
+        [System.String[]]
+        $SenderAddress,
+
+        [System.DateTime]
+        $StartDate,
+
+        [System.String[]]
+        [ValidateSet('None', 'Failed', 'Pending', 'Delivered', 'Expanded')]
+        $Status,
+
+        [System.String]
+        $ToIP
+
+    )
+
+    Begin
+    {
+        $collection = @()
+        $timer = [System.Diagnostics.Stopwatch]::StartNew()
+        [System.Boolean]$haveMore = $true
+        $param = @{}
+        [System.Int16]$PageCounter = '0'
+        if (-not [System.String]::IsNullOrEmpty($EndDate))
+        {
+            $param.Add('EndDate',$EndDate)
+        }
+        if (-not [System.String]::IsNullOrEmpty($Expression))
+        {
+            $param.Add('Expression',$Expression)
+        }
+        if (-not [System.String]::IsNullOrEmpty($FromIP))
+        {
+            $param.Add('FromIP',$FromIP)
+        }
+        if (-not [System.String]::IsNullOrEmpty($MessageId))
+        {
+            $param.Add('MessageId',$MessageId)
+        }
+        if (-not [System.String]::IsNullOrEmpty($MessageTraceId))
+        {
+            $param.Add('MessageTraceId',$MessageTraceId)
+        }
+        if (-not [System.String]::IsNullOrEmpty($PageSize))
+        {
+            $param.Add('PageSize',$PageSize)
+        }
+        if (-not [System.String]::IsNullOrEmpty($ProbeTag))
+        {
+            $param.Add('ProbeTag',$ProbeTag)
+        }
+        if (-not [System.String]::IsNullOrEmpty($RecipientAddress))
+        {
+            $param.Add('RecipientAddress',$RecipientAddress)
+        }
+        if (-not [System.String]::IsNullOrEmpty($SenderAddress))
+        {
+            $param.Add('SenderAddress',$SenderAddress)
+        }
+        if (-not [System.String]::IsNullOrEmpty($StartDate))
+        {
+            $param.Add('StartDate',$StartDate)
+        }
+        if (-not [System.String]::IsNullOrEmpty($Status))
+        {
+            $param.Add('Status',$Status)
+        }
+        if (-not [System.String]::IsNullOrEmpty($ToIP))
+        {
+            $param.Add('ToIP',$ToIP)
+        }
+        $param.Add('Page',[System.Int16]'1')
+    }
+    Process
+    {
+        while ($haveMore) {
+            $tempResult = $null
+            $tempResult = Get-MessageTrace @param
+            $collection += $tempResult
+            Write-Verbose "TotalCount:$($collection.Count) Page:$($param.Page) Runtime:$($timer.Elapsed.ToString()) ResultCount:$($tempResult.Count)"
+            if ($tempResult.Count -eq $PageSize)
+            {
+                Write-Verbose "Increasing Page number"
+                $param.Page++
+            }
+            else
+            {
+                $haveMore = $false
+            }
+        }
+    }
+    End
+    {
+        $collection | Sort-Object Received
+        $timer.Stop()
+        Write-Verbose "ScriptRuntime:$($timer.Elapsed.ToString())"
+    }
+
+}
+
+function Prompt
+{
+    $Host.UI.RawUI.WindowTitle = (Get-Date -UFormat '%y/%m/%d %R').Tostring() + " Connected to EXO as $((Get-PSSession ).Runspace.ConnectionInfo.Credential.UserName)"
+    Write-Host '[' -NoNewline
+    Write-Host (Get-Date -UFormat '%T')-NoNewline
+    Write-Host ']:' -NoNewline
+    Write-Host (Split-Path (Get-Location) -Leaf) -NoNewline
+    return "> "
+}
+Prompt
+
+function Get-ManagedFolderAssistantLog
+{
+    [CmdletBinding()]
+    param(
+        [System.String[]]
+        $Identity
+    )
+
+    Begin
+    {
+        $timer = [System.Diagnostics.Stopwatch]::StartNew()
+        $collection = @()
+    }
+
+    Process
+    {
+        foreach ($ID in $Identity)
+        {
+            Write-Verbose "Processing $($ID)..."
+            $data = New-Object -TypeName PSObject
+            $data | add-member -type NoteProperty -Name Identity -Value $ID
+            $data | add-member -type NoteProperty -Name Ecl -Value $(([xml](Export-MailboxDiagnosticLogs -Identity $ID -ExtendedProperties).MailboxLog).Properties.MailboxTable.Property | ? name -Like 'elc*')
+            $collection += $data
+        }
+    }
+
+    End
+    {
+        $collection
+        $timer.Stop()
+        Write-Verbose "ScriptRuntime:$($timer.Elapsed.ToString())"
+    }
+}
+
+function Get-QuarantineMessageFull
+{
+    [CmdletBinding()]
+    param(
+        [System.String]
+        $Direction,
+
+        [System.String[]]
+        $Domain,
+
+        [System.DateTime]
+        $EndExpiresDate,
+
+        [System.DateTime]
+        $EndReceivedDate,
+
+        [System.String]
+        $Identity,
+
+        [System.String]
+        $MessageId,
+
+        [System.Management.Automation.SwitchParameter]
+        $MyItems,
+
+        [System.Int32]
+        $Page = '1',
+
+        [System.Int32]
+        $PageSize = '1000',
+
+        [System.String[]]
+        [ValidateSet('Bulk', 'Phish', 'Spam', 'Malware', 'TransportRule')]
+        $QuarantineTypes,
+
+        [System.String[]]
+        $RecipientAddress,
+
+        [System.Boolean]
+        $Reported,
+
+        [System.String[]]
+        $SenderAddress,
+
+        [System.DateTime]
+        $StartExpiresDate,
+
+        [System.DateTime]
+        $StartReceivedDate,
+
+        [System.String]
+        $Subject,
+
+        [System.String]
+        [ValidateSet('Bulk', 'Phish', 'Spam', 'TransportRule')]
+        $Type
+
+    )
+
+    Begin
+    {
+        $collection = @()
+        $timer = [System.Diagnostics.Stopwatch]::StartNew()
+        [System.Boolean]$haveMore = $true
+        $param = @{}
+        if (-not [System.String]::IsNullOrEmpty($Direction))
+        {
+            $param.Add('Direction',$Direction)
+        }
+        if (-not [System.String]::IsNullOrEmpty($Domain))
+        {
+            $param.Add('Domain',$($Domain -join ','))
+        }
+        if (-not [System.String]::IsNullOrEmpty($EndExpiresDate))
+        {
+            $param.Add('EndExpiresDate',$EndExpiresDate)
+        }
+        if (-not [System.String]::IsNullOrEmpty($EndReceivedDate))
+        {
+            $param.Add('EndReceivedDate',$EndReceivedDate)
+        }
+        if (-not [System.String]::IsNullOrEmpty($Identity))
+        {
+            $param.Add('Identity',$Identity)
+        }
+        if (-not [System.String]::IsNullOrEmpty($MyItems))
+        {
+            $param.Add('MyItems',$MyItems)
+        }
+        if (-not [System.String]::IsNullOrEmpty($Page))
+        {
+            $param.Add('Page',$Page)
+        }
+        if (-not [System.String]::IsNullOrEmpty($PageSize))
+        {
+            $param.Add('PageSize',$PageSize)
+        }
+        if (-not [System.String]::IsNullOrEmpty($QuarantineTypes))
+        {
+            $param.Add('QuarantineTypes',$($QuarantineTypes -join ','))
+        }
+        if (-not [System.String]::IsNullOrEmpty($RecipientAddress))
+        {
+            $param.Add('RecipientAddress',$($RecipientAddress -join ','))
+        }
+        if (-not [System.String]::IsNullOrEmpty($Reported))
+        {
+            $param.Add('Reported',$Reported)
+        }
+        if (-not [System.String]::IsNullOrEmpty($SenderAddress))
+        {
+            $param.Add('SenderAddress',$($SenderAddress -join ','))
+        }
+        if (-not [System.String]::IsNullOrEmpty($StartExpiresDate))
+        {
+            $param.Add('StartExpiresDate',$StartExpiresDate)
+        }
+        if (-not [System.String]::IsNullOrEmpty($StartReceivedDate))
+        {
+            $param.Add('StartReceivedDate',$StartReceivedDate)
+        }
+        if (-not [System.String]::IsNullOrEmpty($Subject))
+        {
+            $param.Add('Subject',$Subject)
+        }
+        if (-not [System.String]::IsNullOrEmpty($Type))
+        {
+            $param.Add('Type',$Type)
+        }
+    }
+    Process
+    {
+        while ($haveMore) {
+            $tempResult = $null
+            $tempResult = Get-QuarantineMessage @param
+            $collection += $tempResult
+            Write-Verbose "TotalCount:$($collection.Count) Page:$($param.Page) Runtime:$($timer.Elapsed.ToString()) ResultCount:$($tempResult.Count)"
+            if ($tempResult.Count -eq $PageSize)
+            {
+                Write-Verbose "Increasing Page number"
+                $param.Page++
+            }
+            else
+            {
+                $haveMore = $false
+            }
+        }
+    }
+    End
+    {
+        $collection | Sort-Object Received
+        $timer.Stop()
+        Write-Verbose "ScriptRuntime:$($timer.Elapsed.ToString())"
+    }
+
+}
+
+function Test-ExchangeAuditSetting
+{
+[CmdletBinding()]
+param(
+    [Parameter(
+        ValueFromPipeline=$true,
+        Mandatory=$true,
+        Position=0)]
+    [System.Object[]]
+    $Mailbox,
+
+    [Parameter(
+        Mandatory=$false,
+        Position=1)]
+    [System.String[]]
+    $AuditOwnerDesired = @("Update","MoveToDeletedItems","SoftDelete","HardDelete","MailboxLogin","UpdateFolderPermissions","UpdateInboxRules","UpdateCalendarDelegation"),
+
+    [Parameter(
+        Mandatory=$false,
+        Position=2)]
+    [System.String[]]
+    $AuditDelegateDesired = @("Update","MoveToDeletedItems","SoftDelete","HardDelete","SendAs","SendOnBehalf","Create","UpdateFolderPermissions","UpdateInboxRules")
+)
+
+Begin
+{
+
+    $collection = [System.Collections.ArrayList]@()
+    $toBeProcessed = [System.Collections.ArrayList]@()
+    $timer = [System.Diagnostics.Stopwatch]::StartNew()
+    <#
+        .SYNOPSIS
+            Takes an array of strings and converts each element in the array to
+            all lowercase characters.
+    
+        .PARAMETER Array
+            The array of System.String objects to convert into lowercase strings.
+    #>
+    function Convert-StringArrayToLowerCase
+    {
+        [CmdletBinding()]
+        [OutputType([System.String[]])]
+        param
+        (
+            [Parameter()]
+            [System.String[]]
+            $Array
+        )
+    
+        [System.String[]] $arrayOut = New-Object -TypeName 'System.String[]' -ArgumentList $Array.Count
+    
+        for ($i = 0; $i -lt $Array.Count; $i++)
+        {
+            $arrayOut[$i] = $Array[$i].ToLower()
+        }
+    
+        return $arrayOut
+    }
+    
+    <#
+        .SYNOPSIS
+            Returns whether two string arrays have the same contents, where element
+            order doesn't matter.
+    
+        .PARAMETER Array1
+            The first System.String[] object to compare.
+    
+        .PARAMETER Array2
+            The second System.String[] object to compare.
+    
+        .PARAMETER IgnoreCase
+            Specifies that case should be ignored when comparing array contents.
+    #>
+    function Compare-ArrayContent
+    {
+        [CmdletBinding()]
+        [OutputType([System.Boolean])]
+        param
+        (
+            [Parameter()]
+            [System.String[]]
+            $Array1,
+    
+            [Parameter()]
+            [System.String[]]
+            $Array2,
+    
+            [Parameter()]
+            [System.Management.Automation.SwitchParameter]
+            $IgnoreCase
+        )
+    
+        $hasSameContents = $true
+    
+        if ($Array1.Length -ne $Array2.Length)
+        {
+            $hasSameContents = $false
+        }
+        elseif ($Array1.Count -gt 0 -and $Array2.Count -gt 0)
+        {
+            if ($IgnoreCase -eq $true)
+            {
+                $Array1 = Convert-StringArrayToLowerCase -Array $Array1
+                $Array2 = Convert-StringArrayToLowerCase -Array $Array2
+            }
+    
+            foreach ($str in $Array1)
+            {
+                if (!($Array2.Contains($str)))
+                {
+                    $hasSameContents = $false
+                    break
+                }
+            }
+        }
+    
+        return $hasSameContents
+    }
+
+    [System.Int32]$i='1'
+
+}
+
+Process
+{
+
+    foreach($ID in $Mailbox)
+    {
+        $toBeProcessed.Add($ID) | Out-Null
+    }
+
+}
+
+End{
+
+    foreach($ID in $toBeProcessed)
+    {
+        Write-Progress -id 1 -Activity "Processing mailbox - $($ID.PrimarySmtpAddress)" -PercentComplete ( $i / $toBeProcessed.count * 100) -Status "Remaining objects: $($toBeProcessed.count - $i)"
+        
+        $data = New-Object -TypeName PSObject
+        $data | add-member -type NoteProperty -Name Mailbox -Value $($ID.PrimarySmtpAddress)
+        #Write-Verbose "Processing $($ID.PrimarySmtpAddress)..."
+        if(-not [System.String]::IsNullOrEmpty($ID.AuditOwner))
+        {
+            $data | add-member -type NoteProperty -Name AuditOwner -Value $(Compare-ArrayContent -Array1 $AuditOwnerDesired -Array2 $ID.AuditOwner -IgnoreCase)
+        }
+        else
+        {
+            $data | add-member -type NoteProperty -Name AuditOwner -Value 'N/A'
+        }
+
+        if(-not [System.String]::IsNullOrEmpty($ID.AuditDelegate))
+        {
+            $data | add-member -type NoteProperty -Name AuditDelegate -Value $(Compare-ArrayContent -Array1 $AuditOwnerDesired -Array2 $ID.AuditOwner -IgnoreCase)
+        }
+        else
+        {
+            $data | add-member -type NoteProperty -Name AuditDelegate -Value 'N/A'
+        }
+
+        $collection.Add($data) | Out-Null
+        $i++
+    }
+
+    Write-Progress -Activity "Processing mailbox - $($ID.PrimarySmtpAddress)" -Status "Ready" -Completed
+
+    $timer.Stop()
+    Write-Verbose "ScriptRuntime:$($timer.Elapsed.ToString())"
+    $collection
+}
+
+}
